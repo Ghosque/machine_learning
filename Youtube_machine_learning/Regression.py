cce1a0bc2ab36f50ec8421079252a0a03e5f6a0a
aisong_data_processing <yg247350078@163.com>
2018-10-08 17:31:27 +0800

    linear regression simple project and predict some value

--- Youtube_machine_learning/Regression.py
+++ Youtube_machine_learning/Regression.py
@@ -7,9 +7,9 @@ from sklearn import preprocessing, cross_validation, svm
 from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 from matplotlib import style
+import pickle
 
 style.use('ggplot')
-
 
 df = quandl.get('WIKI/GOOGL')
 
@@ -37,8 +37,14 @@ y = np.array(df['label'])
 
 X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
 
-clf = LinearRegression(n_jobs=-1)
-clf.fit(X_train, y_train)
+# clf = LinearRegression(n_jobs=-1)
+# clf.fit(X_train, y_train)
+# with open('linearregression.pickle', 'wb') as f:
+#     pickle.dump(clf, f)
+
+pickle_in = open('linearregression.pickle', 'rb')
+clf = pickle.load(pickle_in)
+
 accuracy = clf.score(X_test, y_test)
 
 forecast_set = clf.predict(X_lately)
