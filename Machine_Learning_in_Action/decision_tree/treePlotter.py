5708a46a38ac216664270e144157b12382c7e25e
aisong_data_processing <yg247350078@163.com>
2019-02-15 18:24:09 +0800

    决策树及树可视化 书P46，wordP65

---
+++ Machine_Learning_in_Action/decision_tree/treePlotter.py
@@ -0,0 +1,70 @@
+import matplotlib
+import matplotlib.pyplot as plt
+
+# 定义自定义字体，文件名是系统中文字体
+myfont = matplotlib.font_manager.FontProperties(fname=r'C:\Windows\Fonts\simkai.ttf')
+# 解决负号'-'显示为方块的问题
+matplotlib.rcParams['axes.unicode_minus'] = False
+
+decisionNode = dict(boxstyle="sawtooth", fc="0.8")
+leafNode = dict(boxstyle="round4", fc="0.8")
+arrow_args = dict(arrowstyle="<-")
+
+
+def plotNode(nodeTxt, centerPt, parentPt, nodeType):
+    createPlot.ax1.annotate(nodeTxt, xy=parentPt, xycoords='axes fraction',
+                            xytext=centerPt, textcoords='axes fraction',
+                            va='center', ha='center', bbox=nodeType,
+                            arrowprops=arrow_args, fontproperties=myfont)
+
+
+def createPlot():
+    fig = plt.figure(1, facecolor='white')
+    fig.clf()
+    createPlot.ax1 = plt.subplot(111, frameon=False)
+    plotNode('决策节点', (0.5, 0.1), (0.1, 0.5), decisionNode)
+    plotNode('叶节点', (0.8, 0.1), (0.3, 0.8), leafNode)
+    plt.show()
+
+
+def getNumLeafs(myTree):
+    numLeafs = 0
+    firstStr = list(myTree.keys())[0]
+    secondDict = myTree[firstStr]
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__ == 'dict':
+            numLeafs += getNumLeafs(secondDict[key])
+        else:
+            numLeafs += 1
+
+    return numLeafs
+
+
+def getTreeDepth(myTree):
+    maxDepth = 0
+    firstStr = list(myTree.keys())[0]
+    secondDict = myTree[firstStr]
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__ == 'dict':
+            thisDepth = 1 + getTreeDepth(secondDict[key])
+        else:
+            thisDepth = 1
+        if thisDepth > maxDepth:
+            maxDepth = thisDepth
+
+    return maxDepth
+
+
+def retrieveTree(i):
+    listOfTrees = [{'no surfacing': {0: 'no', 1: {'flippers': {0: 'no', 1: 'yes'}}}},
+                   {'no surfacing': {0: 'no', 1: {'flippers': {0: {'head': {0: 'no', 1: 'yes'}}, 1: 'no'}}}}]
+    return listOfTrees[i]
+
+
+if __name__ == '__main__':
+    myTree = retrieveTree(0)
+    num = getNumLeafs(myTree)
+    depth = getTreeDepth(myTree)
+    print(num, depth)
+
+
---
+++ Machine_Learning_in_Action/decision_tree/trees.py
@@ -0,0 +1,114 @@
+import math
+import operator
+import matplotlib.pyplot as plt
+
+
+def createDataSet():
+    dataSet = [[1, 1, 'yes'],
+               [1, 1, 'yes'],
+               [1, 0, 'no'],
+               [0, 1, 'no'],
+               [0, 1, 'no']]
+    labels = ['no surfacing', 'flippers']
+    return dataSet, labels
+
+
+def calcShannonEnt(dataSet):
+    """
+    计算香农熵
+    """
+    numEntries = len(dataSet)
+    labelCounts = {}
+    for featVec in dataSet:
+        currentLabel = featVec[-1]
+        if currentLabel not in labelCounts.keys():
+            labelCounts[currentLabel] = 0
+        labelCounts[currentLabel] += 1
+    shannonEnt = 0.0
+    for key in labelCounts:
+        prob = float(labelCounts[key]/numEntries)
+        shannonEnt -= prob * math.log(prob, 2)
+
+    return shannonEnt
+
+
+def splitDataSet(dataSet, axis, value):
+    """
+    划分数据集
+    :param dataSet: 数据集
+    :param axis: 划分特征所在列
+    :param value: 需要返回的特征的值
+    """
+    retDataSet = []
+    for featVec in dataSet:
+        # 抽出符合条件的值
+        if featVec[axis] == value:
+            reducedFeatVec = featVec[:axis]
+            reducedFeatVec.extend(featVec[axis+1:])
+            retDataSet.append(reducedFeatVec)
+
+    return retDataSet
+
+
+def chooseBestFeatureToSplit(dataSet):
+    """
+    选择最好的数据集划分方式
+    """
+    numFeatures = len(dataSet[0]) - 1
+    # 原始香农熵
+    baseEntropy = calcShannonEnt(dataSet)
+    bestInfoGain = 0.0
+    bestFeature = -1
+    for i in range(numFeatures):
+        featList = [example[i] for example in dataSet]
+        uniqueVals = set(featList)
+        newEntropy = 0.0
+        for value in uniqueVals:
+            subDataSet = splitDataSet(dataSet, i, value)
+            prob = len(subDataSet) / float(len(dataSet))
+            newEntropy += prob * calcShannonEnt(subDataSet)
+        infoGain = baseEntropy - newEntropy
+        if infoGain > bestInfoGain:
+            bestInfoGain = infoGain
+            bestFeature = i
+
+    return bestFeature
+
+
+def majorityCnt(classList):
+    classCount = {}
+    for vote in classList:
+        if vote not in classCount.keys():
+            classCount[vote] = 0
+        classCount[vote] += 1
+    sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
+    return sortedClassCount[0][0]
+
+
+def createTree(dataSet, labels):
+    classList = [example[-1] for example in dataSet]
+    if classList.count(classList[0]) == len(classList):
+        return classList[0]
+    if len(dataSet[0]) == 1:
+        return majorityCnt(classList)
+    bestFeat = chooseBestFeatureToSplit(dataSet)
+    bestFeatLabel = labels[bestFeat]
+    myTree = {bestFeatLabel: {}}
+    del(labels[bestFeat])
+    featValues = [example[bestFeat] for example in dataSet]
+    uniqueVals = set(featValues)
+    for value in uniqueVals:
+        subLabels = labels[:]
+        myTree[bestFeatLabel][value] = createTree(splitDataSet(dataSet, bestFeat, value), subLabels)
+
+    return myTree
+
+
+if __name__ == '__main__':
+    myDat, labels = createDataSet()
+    myTree = createTree(myDat, labels)
+    print(myTree)
+
+
+
+
