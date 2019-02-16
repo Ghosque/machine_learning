234e165808356de8f522b9bac3041b11f5f97ce5
aisong_data_processing <yg247350078@163.com>
2019-02-16 17:45:59 +0800

    画出决策树树形图

--- Machine_Learning_in_Action/decision_tree/treePlotter.py
+++ Machine_Learning_in_Action/decision_tree/treePlotter.py
@@ -18,13 +18,13 @@ def plotNode(nodeTxt, centerPt, parentPt, nodeType):
                             arrowprops=arrow_args, fontproperties=myfont)
 
 
-def createPlot():
-    fig = plt.figure(1, facecolor='white')
-    fig.clf()
-    createPlot.ax1 = plt.subplot(111, frameon=False)
-    plotNode('决策节点', (0.5, 0.1), (0.1, 0.5), decisionNode)
-    plotNode('叶节点', (0.8, 0.1), (0.3, 0.8), leafNode)
-    plt.show()
+# def createPlot():
+#     fig = plt.figure(1, facecolor='white')
+#     fig.clf()
+#     createPlot.ax1 = plt.subplot(111, frameon=False)
+#     plotNode('决策节点', (0.5, 0.1), (0.1, 0.5), decisionNode)
+#     plotNode('叶节点', (0.8, 0.1), (0.3, 0.8), leafNode)
+#     plt.show()
 
 
 def getNumLeafs(myTree):
@@ -61,10 +61,46 @@ def retrieveTree(i):
     return listOfTrees[i]
 
 
+def plotMidText(cntrPt, parentPt, txtString):
+    xMid = (parentPt[0] - cntrPt[0]) / 2.0 + cntrPt[0]
+    yMid = (parentPt[1] - cntrPt[1]) / 2.0 + cntrPt[1]
+    createPlot.ax1.text(xMid, yMid, txtString)
+
+
+def plotTree(myTree, parentPt, nodeTxt):
+    numLeafs = getNumLeafs(myTree)
+    depth = getTreeDepth(myTree)
+    firstStr = list(myTree.keys())[0]
+    cntrPt = (plotTree.xOff + (1.0 + float(numLeafs)) / 2.0 / plotTree.totalW, plotTree.yOff)
+    plotMidText(cntrPt, parentPt, nodeTxt)
+    plotNode(firstStr, cntrPt, parentPt, decisionNode)
+    secondDict = myTree[firstStr]
+    plotTree.yOff = plotTree.yOff - 1.0/plotTree.totalD
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__ == 'dict':
+            plotTree(secondDict[key], cntrPt, str(key))
+        else:
+            plotTree.xOff = plotTree.xOff + 1.0/plotTree.totalW
+            plotNode(secondDict[key], (plotTree.xOff, plotTree.yOff), cntrPt, leafNode)
+            plotMidText((plotTree.xOff, plotTree.yOff), cntrPt, str(key))
+    plotTree.yOff = plotTree.yOff + 1.0/plotTree.totalD
+
+
+def createPlot(inTree):
+    fig = plt.figure(1, facecolor='white')
+    fig.clf()
+    axprops = dict(xticks=[], yticks=[])
+    createPlot.ax1 = plt.subplot(111, frameon=False, **axprops)
+    plotTree.totalW = float(getNumLeafs(inTree))
+    plotTree.totalD = float(getTreeDepth(inTree))
+    plotTree.xOff = -0.5 / plotTree.totalW
+    plotTree.yOff = 1.0
+    plotTree(inTree, (0.5, 1.0), '')
+    plt.show()
+
+
 if __name__ == '__main__':
     myTree = retrieveTree(0)
-    num = getNumLeafs(myTree)
-    depth = getTreeDepth(myTree)
-    print(num, depth)
+    createPlot(myTree)
 
 
