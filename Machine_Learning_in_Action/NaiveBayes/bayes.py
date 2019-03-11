c2b156058d4ba4e2c432795f18ff3c5960a9a590
ghosque <yg247350078@163.com>
2019-03-11 21:55:20 +0800

    update

--- Machine_Learning_in_Action/NaiveBayes/bayes.py
+++ Machine_Learning_in_Action/NaiveBayes/bayes.py
@@ -1,3 +1,5 @@
+import random
+import feedparser
 import numpy as np
 
 
@@ -79,6 +81,118 @@ def testingNB():
     print(testEntry, 'classified as: ', classifyNB(thisDoc, p0V, p1V, pAb))
 
 
+def bagOfWord2VecMN(vocabList, inputSet):
+    returnVec = [0] * len(vocabList)
+    for word in inputSet:
+        if word in vocabList:
+            returnVec[vocabList.index(word)] += 1
+
+    return returnVec
+
+
+def textParse(bigString):
+    """准备数据"""
+    listOfTokens = [i.replace('.', '').replace(',', '') for i in bigString.split(' ')]
+    return [tok.lower() for tok in listOfTokens if len(tok) > 2]
+
+
+def spamTest():
+    """测试算法：留存交叉验证"""
+    docList = []
+    classList = []
+    fullText = []
+    for i in range(25):
+        wordList = textParse(open('email/spam/{}.txt'.format(i+1)).read())
+        docList.append(wordList)
+        fullText.extend(wordList)
+        classList.append(1)
+        wordList = textParse(open('email/ham/{}.txt'.format(i+1)).read())
+        docList.append(wordList)
+        fullText.extend(wordList)
+        classList.append(0)
+
+    vocabList = createVocabList(docList)
+    trainingSet = list(range(50))
+    testSet = []
+    for i in range(10):
+        # 随机取一个实数
+        randIndex = int(random.uniform(0, len(trainingSet)))
+        testSet.append(trainingSet[randIndex])
+        del(trainingSet[randIndex])
+
+    trainMat = []
+    trainClasses = []
+    # 遍历随机取出的文章
+    for docIndex in trainingSet:
+        trainMat.append(setOfWords2Vec(vocabList, docList[docIndex]))
+        trainClasses.append(classList[docIndex])
+
+    p0V, p1V, pSpam = trainNB0(np.array(trainMat), np.array(trainClasses))
+    errorCount = 0
+    for docIndex in testSet:
+        wordVector = setOfWords2Vec(vocabList, docList[docIndex])
+        if classifyNB(np.array(wordVector), p0V, p1V, pSpam) != classList[docIndex]:
+            errorCount += 1
+
+    print('the error rate is: ', float(errorCount)/len(testSet))
+
+
+def calcMostFreq(vocabList, fullText):
+    import operator
+    freqDict = {}
+    for token in vocabList:
+        freqDict[token] = fullText.count(token)
+    sortedFreq = sorted(freqDict.items(), key=operator.itemgetter(1), reverse=True)
+
+    return sortedFreq[:30]
+
+
+def localWords(feed1, feed0):
+    """RSS源分类器及高频词去除函数"""
+    docList = []
+    classList = []
+    fullText = []
+    minLen = min(len(feed1['entries']), len(feed0['entries']))
+    for i in range(minLen):
+        wordList = textParse(feed1['entries'][i]['summary'])
+        docList.append(wordList)
+        fullText.extend(wordList)
+        classList.append(1)
+        wordList = textParse(feed0['entries'][i]['summary'])
+        docList.append(wordList)
+        fullText.extend(wordList)
+        classList.append(0)
+
+    vocabList = createVocabList(docList)
+    top30Words = calcMostFreq(vocabList, fullText)
+    for pariW in top30Words:
+        if pariW[0] in vocabList:
+            vocabList.remove(pariW[0])
+
+    trainingSet = list(range(2*minLen))
+    testSet = []
+    for i in range(20):
+        randIndex = int(random.uniform(0, len(trainingSet)))
+        testSet.append(trainingSet[randIndex])
+        del(trainingSet[randIndex])
+
+    trainMat = []
+    trainClasses = []
+    for docIndex in trainingSet:
+        trainMat.append(bagOfWord2VecMN(vocabList, docList[docIndex]))
+        trainClasses.append(classList[docIndex])
+
+    p0V, p1V, pSpam = trainNB0(np.array(trainMat), np.array(trainClasses))
+    errorCount = 0
+    for docIndex in testSet:
+        wordVector = bagOfWord2VecMN(vocabList, docList[docIndex])
+        if classifyNB(np.array(wordVector), p0V, p1V, pSpam) != classList[docIndex]:
+            errorCount += 1
+
+    print('the error rate is:', float(errorCount)/len(testSet))
+    return vocabList, p0V, p1V
+
+
 def main():
     listOPosts, listClasses = loadDataset()
     myVocabList = createVocabList(listOPosts)
@@ -94,6 +208,8 @@ def main():
 
 
 if __name__ == '__main__':
-    testingNB()
+    ny = feedparser.parse('http://www.nasa.gov/rss/dyn/image_of_the_day.rss')
+    sf = feedparser.parse('http://sports.yahoo.com/nba/teams/hou/rss.xml')
+    vocabList, pSF, pNY = localWords(ny, sf)
 
 
